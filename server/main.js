5718055a52d6fa155aec62c444433e074943bc07
Dev Dixit <devdixitsocial@gmail.com>
2025-06-22 20:08:51 +0530

    create error handler, security related middlewares and rate limiters

--- server/main.js
+++ server/main.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import 'dotenv/config';
+import helmet from 'helmet';
+import cors from 'cors';
+import xssClean from 'xss-clean';
+import rateLimiter from 'express-rate-limit';
 
 import { Log } from './middlewares/log.js';
 import Database from './config/database.js';
 import authRouter from './routes/auth.route.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,7 +16,14 @@ const port = process.env.PORT || 4000;
 Database();
 
 app.use(Log);
+app.use(errorHandler);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xssClean());
+
+const limiter = rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }); // rate limiter
+app.use(limiter);
 
 app.use('/auth/v1', authRouter);
 
